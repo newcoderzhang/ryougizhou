22ca04a4847fa389768d1a17ee31827f385531fd
zhoutong <ryougizhou@zhoutongdeMacBook-Pro.local>
2015-05-07 01:15:28 +0800

    TestGit

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by zhoutong on 15/5/7.
+//  Copyright (c) 2015年 zhoutong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by zhoutong on 15/5/7.
+//  Copyright (c) 2015年 zhoutong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
